668e9e04ba3211385d1d82e94bad1934c9044b15
Andrii Serkhovetc <andrii.s@datarails.com>
2023-11-30 18:34:48 +0200

    initial commit

---
+++ src/review/review.model.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ReviewDocument = HydratedDocument<ReviewModel>;
+@Schema()
+export class ReviewModel {
+  @Prop()
+  name: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  rating: number;
+
+  @Prop()
+  createdAt: Date;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(ReviewModel);
---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,65 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export enum TopLevelCategories {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export type TopPageModelDocument = HydratedDocument<TopPageModel>;
+
+export class HhData {
+  @Prop()
+  count: number;
+
+  @Prop()
+  juniorSalary: number;
+
+  @Prop()
+  middleSalary: number;
+
+  @Prop()
+  seniorSalary: number;
+}
+
+export class Advantage {
+  @Prop()
+  title: string;
+
+  @Prop()
+  description: string;
+}
+
+@Schema()
+export class TopPageModel {
+  @Prop({ enum: TopLevelCategories })
+  firstCategory: TopLevelCategories;
+
+  @Prop()
+  secondCategory: string;
+
+  @Prop({ unique: true })
+  alias: string;
+
+  @Prop()
+  title: string;
+
+  @Prop(HhData)
+  hh?: HhData;
+
+  @Prop([Advantage])
+  advantages: Advantage[];
+
+  @Prop()
+  seoText: string;
+
+  @Prop()
+  tagsTitle: string;
+
+  @Prop([String])
+  tags: string[];
+}
+
+export const TopPageSchema = SchemaFactory.createForClass(TopPageModel);
